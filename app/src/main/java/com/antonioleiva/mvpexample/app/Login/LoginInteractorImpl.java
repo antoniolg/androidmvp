9a8fa04ecb8fae0f3569d7149788deefdbf8a448
Edwin Hernandez <Evin1-@users.noreply.github.com>
2017-12-11 03:01:34 -0500

    Removing the error boolean in LoginInteractor (#25)
    * Update buildToolsVersion -24.0.3
    
    * Update Gradle wrapper -2.2.2
    
    * Remove unused error variable in Login handler

--- app/src/main/java/com/antonioleiva/mvpexample/app/Login/LoginInteractorImpl.java
+++ app/src/main/java/com/antonioleiva/mvpexample/app/Login/LoginInteractorImpl.java
@@ -10,20 +10,15 @@ public class LoginInteractorImpl implements LoginInteractor {
         // Mock login. I'm creating a handler to delay the answer a couple of seconds
         new Handler().postDelayed(new Runnable() {
             @Override public void run() {
-                boolean error = false;
-                if (TextUtils.isEmpty(username)){
+                if (TextUtils.isEmpty(username)) {
                     listener.onUsernameError();
-                    error = true;
                     return;
                 }
-                if (TextUtils.isEmpty(password)){
+                if (TextUtils.isEmpty(password)) {
                     listener.onPasswordError();
-                    error = true;
                     return;
                 }
-                if (!error){
-                    listener.onSuccess();
-                }
+                listener.onSuccess();
             }
         }, 2000);
     }
