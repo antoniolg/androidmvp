b322afd876ef94cf0735496c782405d9b66ceef8
Antonio Leiva <antonioleivag@gmail.com>
2018-07-04 09:39:21 +0200

    Update license

--- app/src/main/java/com/antonioleiva/mvpexample/app/main/FindItemsInteractor.java
+++ app/src/main/java/com/antonioleiva/mvpexample/app/main/FindItemsInteractor.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright (C) 2014 Antonio Leiva Gordillo.
+ *  * Copyright (C) 2018 Antonio Leiva Gordillo.
  *  *
  *  * Licensed under the Apache License, Version 2.0 (the "License");
  *  * you may not use this file except in compliance with the License.
